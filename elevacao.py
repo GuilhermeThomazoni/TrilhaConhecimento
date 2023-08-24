1c21786e6f793e0e4fc290ed221bbe3aa90b6f87
GuilhermeThomazoni <138123625+GuilhermeThomazoni@users.noreply.github.com>
2023-08-24 14:06:34 -0300

    Add files via upload

---
+++ elevacao.py
@@ -0,0 +1,12 @@
+#Atividade feita no dia 22/08.
+
+def calcular_elevacao(base, expoente):
+    if expoente == 0:
+        return 1
+    else:
+        return base * calcular_elevacao(base, expoente - 1)
+
+base = float(input("Digite a base: "))
+expoente = int(input("Digite o expoente: "))
+resultado = calcular_elevacao(base, expoente)
+print(f"{base} elevado a {expoente} é igual a {resultado}")
---
+++ fatorial.py
@@ -0,0 +1,10 @@
+#Atividade feita no dia 22/08.
+
+def fatorial(x):
+    aux = 1
+    while x > 0:
+        aux = aux*x
+        x=x-1
+    return aux
+    
+print(fatorial(2))
