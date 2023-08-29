87b21f88ac3f3536f7e4665c74a9b62eacfb132a
GuilhermeThomazoni <138123625+GuilhermeThomazoni@users.noreply.github.com>
2023-08-29 15:59:46 -0300

    Add files via upload

---
+++ scriptpastas.sh
@@ -0,0 +1,13 @@
+echo "Insira o nome da primeira pasta."
+read newdir1
+mkdir $newdir1 
+
+echo "Insira o nome da segunda pasta."
+read newdir2
+mkdir $newdir2
+
+echo "Insira o nome da terceira pasta."
+read newdir3
+mkdir $newdir3
+
+echo "Pastas $newdir1 $newdir2 $newdir3 criadas com sucesso."
